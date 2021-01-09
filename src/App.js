3ba7f923091df836caf16338d3e2bd01bcba5032
Kevin <kevin.ayers123@icloud.com>
2021-01-08 17:04:03 -0800

    created homepage and menu-item components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/hompage.component'; 
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
